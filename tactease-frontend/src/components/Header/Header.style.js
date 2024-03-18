671fcd7c5c02a3c2247a9e13b33805134f8e48b4
Adi Levi <adile0194@gmail.com>
2024-03-18 20:12:57 +0200

    logo position

--- tactease-frontend/src/components/Header/Header.style.js
+++ tactease-frontend/src/components/Header/Header.style.js
@@ -3,11 +3,10 @@ import styled from "styled-components";
 export const HeaderStyle = styled.header`
     display: flex;
     flex-flow: row;
-`;
+    margin: 1% 0 0.5% 1%;`;
 
 export const LogoStyle = styled.img`
-    width: 150px;
-    height: 20px;
+    width: 140px;
+    height: 90px;
     display: block;
-    position: relative;
 `;
