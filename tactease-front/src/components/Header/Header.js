de150bd7e64af51ad9713ec5977df432f0fe4798
Adi Levi <adile0194@gmail.com>
2024-03-18 19:23:59 +0200

    init header

---
+++ tactease-front/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { HeaderStyle } from "./Header.style";
+import Logo from "../../assets/logo.png";
+
+
+const Header = ({}) => {
+    return (
+        <HeaderStyle>
+        </HeaderStyle>
+    );
+};
+
+
+export default Header;
---
+++ tactease-front/src/components/Header/Header.style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+const HeaderStyle = styled.header`
+    display: flex;
+    flex-flow: row;
+`;
