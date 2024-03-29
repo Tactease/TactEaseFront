cb8b1fc385d13ae7bacc2d7f68649f02528ef6a8
MoranSin <nurixhbh@gmail.com>
2024-03-29 18:47:00 +0300

    calender progress haha

--- tactease-frontend/src/App.jsx
+++ tactease-frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header/Header.jsx';
+import Calendar from './components/Schedule/Schedule.jsx';
 
 function App() {
   return (
     <div className="App">
       <Header />
+        <Calendar />
     </div>
   );
 }
--- tactease-frontend/src/components/Schedule/Schedule.jsx
+++ tactease-frontend/src/components/Schedule/Schedule.jsx
@@ -1,201 +1,142 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import { ViewState, EditingState } from '@devexpress/dx-react-scheduler';
-import {
-    Scheduler,
-    Toolbar,
-    MonthView,
-    WeekView,
-    ViewSwitcher,
-    Appointments,
-    AppointmentTooltip,
-    AppointmentForm,
-    DragDropProvider,
-    EditRecurrenceMenu,
-    AllDayPanel,
-} from '@devexpress/dx-react-scheduler-material-ui';
-import { connectProps } from '@devexpress/dx-react-core';
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Button from '@mui/material/Button';
-import Fab from '@mui/material/Fab';
-import IconButton from '@mui/material/IconButton';
-import AddIcon from '@mui/icons-material/Add';
-import TextField from '@mui/material/TextField';
-import LocationOn from '@mui/icons-material/LocationOn';
-import Notes from '@mui/icons-material/Notes';
-import Close from '@mui/icons-material/Close';
-import CalendarToday from '@mui/icons-material/CalendarToday';
-import Create from '@mui/icons-material/Create';
+import React, { useState, useRef, useEffect } from 'react';
+import { DayPilot, DayPilotCalendar } from "@daypilot/daypilot-lite-react";
+import "./schedule.style.css"
+import missionsData from '../../demo-data/missions.json';
 
-import { missions } from '../../../demo-data/missions';
-
-const PREFIX = 'Scheduler';
-const classes = {
-    content: `${PREFIX}-content`,
-    header: `${PREFIX}-header`,
-    closeButton: `${PREFIX}-closeButton`,
-    buttonGroup: `${PREFIX}-buttonGroup`,
-    button: `${PREFIX}-button`,
-    picker: `${PREFIX}-picker`,
-    wrapper: `${PREFIX}-wrapper`,
-    icon: `${PREFIX}-icon`,
-    textField: `${PREFIX}-textField`,
-    addButton: `${PREFIX}-addButton`,
+const convertToISO = (dateStr) => {
+    const [date, time] = dateStr.split(' ');
+    const [day, month, year] = date.split('/');
+    let [hours, minutes] = time.split(':');
+    hours = hours.padStart(2, '0');
+    minutes = minutes.padStart(2, '0');
+    return `${year}-${month}-${day}T${hours}:${minutes}:00`;
 };
 
-const StyledDiv = styled('div')(({ theme }) => ({
-    [`& .${classes.icon}`]: {
-        margin: theme.spacing(2, 0),
-        marginRight: theme.spacing(2),
-    },
-    [`& .${classes.header}`]: {
-        overflow: 'hidden',
-        paddingTop: theme.spacing(0.5),
-    },
-    [`& .${classes.textField}`]: {
-        width: '100%',
-    },
-    [`& .${classes.content}`]: {
-        padding: theme.spacing(2),
-        paddingTop: 0,
-    },
-    [`& .${classes.closeButton}`]: {
-        float: 'right',
-    },
-    [`& .${classes.picker}`]: {
-        marginRight: theme.spacing(2),
-        '&:last-child': {
-            marginRight: 0,
-        },
-        width: '50%',
-    },
-    [`& .${classes.wrapper}`]: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        padding: theme.spacing(1, 0),
-    },
-    [`& .${classes.buttonGroup}`]: {
-        display: 'flex',
-        justifyContent: 'flex-end',
-        padding: theme.spacing(0, 2),
-    },
-    [`& .${classes.button}`]: {
-        marginLeft: theme.spacing(2),
-    },
-}));
-const StyledFab = styled(Fab)(({ theme }) => ({
-    [`&.${classes.addButton}`]: {
-        position: 'absolute',
-        bottom: theme.spacing(3),
-        right: theme.spacing(4),
-    },
-}));
+const Calendar = () => {
+    const calendarRef = useRef()
 
-class SchedulerFormContainerBasic extends React.PureComponent {
-    constructor(props) {
-        super(props);
+    const editEvent = async (e) => {
+        const dp = calendarRef.current.control;
+        const modal = await DayPilot.Modal.prompt("Update event text:", e.text());
+        if (!modal.result) { return; }
+        e.data.text = modal.result;
+        dp.events.update(e);
+    };
 
-        this.state = {
-            schedulerChanges: {},
-        };
-
-        this.getSchedulerData = () => {
-            const { missionData } = this.props;
-            return missionData;
-        };
-        this.getSchedulerChanges = () => {
-            const { missionChanges } = this.state;
-            return missionChanges;
-        };
-
-        this.changeMission = this.changeMission.bind(this);
-        this.commitMission = this.commitMission.bind(this);
-    }
-
-    changeMission({ field, changes }) {
-        const nextChanges = {
-            ...this.getSchedulerChanges(),
-            [field]: changes,
-        };
-        this.setState({
-            schedulerChanges: nextChanges,
-        });
-    }
-
-    commitMission(type) {
-        const { commitChanges } = this.props;
-        const mission = {
-            ...this.getSchedulerData(),
-            ...this.getSchedulerChanges(),
-        };
-        if (type === 'deleted') {
-            commitChanges({ [type]: mission._id });
-        } else if (type === 'changed') {
-            commitChanges({ [type]: { [mission._id]: mission } });
-        } else {
-            commitChanges({ [type]: mission });
-        }
-        this.setState({
-            schedulerChanges: {},
-        });
-    }
-
-    render() {
-        const {
-            visible,
-            visibleChange,
-            missionData,
-            cancelMission,
-            target,
-            onHide,
-        } = this.props;
-        const { schedulerChanges } = this.state;
-
-        const displayMissionData = {
-            ...missionData,
-            ...schedulerChanges,
-        };
-
-        const isNewMission = missionData.id === undefined;
-        const applyChanges = isNewMission
-            ? () => this.commitMission('added')
-            : () => this.commitMission('changed');
-
-        const textEditorProps = field => ({
-            variant: 'outlined',
-            onChange: ({ target: change }) => this.changeMission({
-                field: [field], changes: change.value,
-            }),
-            value: displayMissionData[field] || '',
-            label: field[0].toUpperCase() + field.slice(1),
-            className: classes.textField,
-        });
-        const pickerEditorProps = field => ({
-            // keyboard: true,
-            value: displayMissionData[field],
-            onChange: date => this.changeMission({
-                field: [field], changes: date ? date.toDate() : new Date(displayMissionData[field]),
-            }),
-            ampm: false,
-            inputFormat: 'DD/MM/YYYY HH:mm',
-            onError: () => null,
-        });
-
-        const startDatePickerProps = pickerEditorProps('startDate');
-        const endDatePickerProps = pickerEditorProps('endDate');
-        const cancelChanges = () => {
-            this.setState({
-                schedulerChanges: {},
+    const [calendarConfig, setCalendarConfig] = useState({
+        viewType: "Week",
+        durationBarVisible: false,
+        eventHeight: 50,
+        timeRangeSelectedHandling: "Enabled",
+        onTimeRangeSelected: async args => {
+            const dp = calendarRef.current.control;
+            const modal = await DayPilot.Modal.prompt("Create a new event:", "Event 1");
+            dp.clearSelection();
+            if (!modal.result) { return; }
+            dp.events.add({
+                start: args.start,
+                end: args.end,
+                id: DayPilot.guid(),
+                text: modal.result
             });
-            visibleChange();
-            cancelMission();
-        };
-        
+        },
+        onEventClick: async args => {
+            await editEvent(args.e);
+        },
+        contextMenu: new DayPilot.Menu({
+            items: [
+                {
+                    text: "Delete",
+                    onClick: async args => {
+                        const dp = calendarRef.current.control;
+                        dp.events.remove(args.source);
+                    },
+                },
+                {
+                    text: "-"
+                },
+                {
+                    text: "Edit...",
+                    onClick: async args => {
+                        await editEvent(args.source);
+                    }
+                }
+            ]
+        }),
+        onBeforeEventRender: args => {
+            args.data.areas = [
+                {
+                    top: 3,
+                    right: 3,
+                    width: 20,
+                    height: 20,
+                    symbol: "icons/daypilot.svg#minichevron-down-2",
+                    fontColor: "#fff",
+                    toolTip: "Show context menu",
+                    action: "ContextMenu",
+                },
+                {
+                    top: 3,
+                    right: 25,
+                    width: 20,
+                    height: 20,
+                    symbol: "icons/daypilot.svg#x-circle",
+                    fontColor: "#fff",
+                    action: "None",
+                    toolTip: "Delete event",
+                    onClick: async args => {
+                        const dp = calendarRef.current.control;
+                        dp.events.remove(args.source);
+                    }
+                }
+            ];
+
+
+            // const participants = args.data.participants;
+            // if (participants > 0) {
+            //     // show one icon for each participant
+            //     for (let i = 0; i < participants; i++) {
+            //         args.data.areas.push({
+            //             bottom: 5,
+            //             right: 5 + i * 30,
+            //             width: 24,
+            //             height: 24,
+            //             action: "None",
+            //             image: `https://picsum.photos/24/24?random=${i}`,
+            //             style: "border-radius: 50%; border: 2px solid #fff; overflow: hidden;",
+            //         });
+            //     }
+            // }
+        }
+    });
+
+    useEffect(() => {
+        const events = missionsData.map((mission) => ({
+            id: mission.missionId,
+            text: mission.missionType,
+            start: convertToISO(mission.startDate),
+            end: convertToISO(mission.endDate),
+            backColor: "#6aa84f",
+            participants: mission.soldierCount,
+        }));
+
+        const startDate = "2024-02-12";
+
+        calendarRef.current.control.update({startDate, events});
+    }, []);
+
+    return (
+        <div className="wrap">
+             <main>
+
+                <DayPilotCalendar
+                    {...calendarConfig}
+                    ref={calendarRef}
+                />
+             </main>
+         </div>
+    );
+}
+
+export default Calendar;
+
