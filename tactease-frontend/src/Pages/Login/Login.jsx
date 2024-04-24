8606b6153416b586b93606a87791ae3371bb85ea
Adi Levi <adile0194@gmail.com>
2024-04-24 14:01:57 +0300

    added routs and login form static

--- tactease-frontend/src/Pages/Login/Login.jsx
+++ tactease-frontend/src/Pages/Login/Login.jsx
@@ -1,8 +1,19 @@
 import PageTitle from "../../components/PageTitle/PageTitle.jsx";
+import {LoginContainer, LoginForm} from './Login.styled';
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
 
 const Login = () => {
     return (
-        <PageTitle title="Login"/>
+        <LoginContainer>
+            <PageTitle title="Login"/>
+            <LoginForm>
+                <FormControl>
+                <TextField id="standard-basic" label="Personal Number" variant="standard"/>
+                </FormControl>
+                <TextField id="standard-basic" label="Standard" variant="standard"/>
+            </LoginForm>
+        </LoginContainer>
     );
 }
 export default Login;
