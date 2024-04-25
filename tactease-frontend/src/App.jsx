22129b8e1b3e34711c82f6696d93146e48468149
MoranSin <nurixhbh@gmail.com>
2024-04-25 19:01:42 +0300

    login fixed baroh hashem

--- tactease-frontend/src/App.jsx
+++ tactease-frontend/src/App.jsx
@@ -1,22 +1,35 @@
 import Header from './components/Header/Header.jsx';
+import { useEffect } from "react";
 import CalenderPage from './Pages/CalenderPage/CalenderPage.jsx';
 import Login from "./Pages/Login/Login.jsx";
 import AddMissions from "./Pages/AddMissions/AddMissions.jsx";
-import {Navigate, Route, Routes} from 'react-router-dom'
+import {Route, Routes, useNavigate} from 'react-router-dom'
 
 function App() {
     const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null;
     console.log("User value:", user); // Log the user value
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (user) {
+            navigate('/');
+        }
+        else{
+            navigate('/login');
+        }
+    }, []);
+
 
     return (
         <div className="App">
             <Header/>
             <Routes>
-                <Route exact path="/" element={user ? <CalenderPage /> : <Navigate to="/login" />} />
-                <Route path="/login" element={user ? <Navigate to="/" /> : <Login/>}/>
-                <Route path="/addMissions" element={user ? <AddMissions /> : <Navigate to="/login" />}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route exact path="/" element={<CalenderPage/>}/>
+                <Route path="/addMissions" element={<AddMissions/>}/>
             </Routes>
         </div>
     );
 }
+
 export default App;
