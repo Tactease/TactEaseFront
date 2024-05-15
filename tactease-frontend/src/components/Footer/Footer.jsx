2ae8bf9ad3dd4af9b5d8d74e44844e3aac323ace
MoranSin <nurixhbh@gmail.com>
2024-05-15 17:35:50 +0300

    footer navigation works

--- tactease-frontend/src/components/Footer/Footer.jsx
+++ tactease-frontend/src/components/Footer/Footer.jsx
@@ -8,19 +8,29 @@ import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
 import QuestionAnswerOutlinedIcon from '@mui/icons-material/QuestionAnswerOutlined';
 import Logout from '@mui/icons-material/Logout';
+import {Link, useNavigate} from "react-router-dom";
 
 const Footer = () => {
 
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        localStorage.removeItem('user');
+        navigate('/login');
+    }
 
     return (
         <FooterContainer>
+            <Link to="requests">
             <IconButton aria-label="delete" size="large">
                 <QuestionAnswerIcon />
             </IconButton>
-            <IconButton aria-label="delete" size="large" >
-                <HomeIcon />
-            </IconButton>
-            <IconButton aria-label="logout" size="large">
+            </Link>
+            <Link to="/">
+                <IconButton aria-label="delete" size="large" >
+                    <HomeIcon />
+                </IconButton>
+            </Link>
+            <IconButton aria-label="logout" size="large" onClick={handleLogout}>
                 <Logout />
             </IconButton>
         </FooterContainer>
